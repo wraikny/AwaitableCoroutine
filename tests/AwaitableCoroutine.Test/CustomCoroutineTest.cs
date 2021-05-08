1ac5a88d004bcc38041cdeb1c464efd28a5ad4ce
wraikny <writtenknown@gmail.com>
2021-05-09 01:53:36 +0900

    add custom coroutine test

---
+++ tests/AwaitableCoroutine.Test/CustomCoroutineTest.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AwaitableCoroutine.Test
+{
+    public class CustomCoroutineTest : TestTemplate
+    {
+        public CustomCoroutineTest(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+
+        }
+
+        private sealed class CustomCoroutine : AwaitableCoroutine
+        {
+            public CustomCoroutine() { }
+
+            protected override void OnMoveNext()
+            {
+                Complete();
+            }
+        }
+
+        [Fact]
+        public void RunCustomCoroutine()
+        {
+            var runner = new CoroutineRunner();
+
+            var c = runner.AddCoroutine(() => new CustomCoroutine());
+            
+            Assert.False(c.IsCompleted);
+
+            runner.Update();
+            
+            Assert.True(c.IsCompleted);
+        }
+    }
+}
