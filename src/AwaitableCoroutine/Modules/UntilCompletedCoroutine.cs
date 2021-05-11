2fbad97f848e9d71036a1c142a9e62ce5f76393c
wraikny <writtenknown@gmail.com>
2021-05-12 02:13:35 +0900

    add UntilCompletedCoroutine of await AwaitableCoroutine

--- src/AwaitableCoroutine/Modules/UntilCompletedCoroutine.cs
+++ src/AwaitableCoroutine/Modules/UntilCompletedCoroutine.cs
@@ -6,6 +6,11 @@ namespace AwaitableCoroutine
     {
         public static async AwaitableCoroutine UntilCompleted(this AwaitableCoroutineBase coroutine, Action action)
         {
+            if (coroutine is null)
+            {
+                ThrowHelper.ArgNull(nameof(coroutine));
+            }
+
             if (action is null)
             {
                 ThrowHelper.ArgNull(nameof(action));
@@ -19,6 +24,50 @@ namespace AwaitableCoroutine
                 }
                 action.Invoke();
                 await AwaitableCoroutine.Yield();
+            }
+        }
+
+        public static async AwaitableCoroutine UntilCompleted(this AwaitableCoroutineBase coroutine, Func<AwaitableCoroutine> createCoroutine)
+        {
+            if (coroutine is null)
+            {
+                ThrowHelper.ArgNull(nameof(coroutine));
+            }
+
+            if (createCoroutine is null)
+            {
+                ThrowHelper.ArgNull(nameof(createCoroutine));
+            }
+
+            while (!coroutine.IsCompleted)
+            {
+                if (coroutine.IsCanceled)
+                {
+                    AwaitableCoroutine.ThrowChildCancel<AwaitableCoroutineBase>(coroutine);
+                }
+                await createCoroutine.Invoke();
+            }
+        }
+
+        public static async AwaitableCoroutine UntilCompleted<T>(this AwaitableCoroutineBase coroutine, Func<AwaitableCoroutine<T>> action)
+        {
+            if (coroutine is null)
+            {
+                ThrowHelper.ArgNull(nameof(coroutine));
+            }
+
+            if (action is null)
+            {
+                ThrowHelper.ArgNull(nameof(action));
+            }
+
+            while (!coroutine.IsCompleted)
+            {
+                if (coroutine.IsCanceled)
+                {
+                    AwaitableCoroutine.ThrowChildCancel<AwaitableCoroutineBase>(coroutine);
+                }
+                _ = await action.Invoke();
             }
         }
     }
