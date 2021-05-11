54bbf6c6c1802c15d79766acb7209293978ff8ef
wraikny <writtenknown@gmail.com>
2021-05-12 03:32:32 +0900

    update WaitingCoroutineRegisterNotifier

--- src/AwaitableCoroutine/Internal/WaitingCoroutineRegisterNotifier.cs
+++ src/AwaitableCoroutine/Internal/WaitingCoroutineRegisterNotifier.cs
@@ -1,4 +1,6 @@
-﻿namespace AwaitableCoroutine.Internal
+﻿using System.Collections.Generic;
+
+namespace AwaitableCoroutine.Internal
 {
     public interface IWaitingCoroutineRegisterer
     {
@@ -9,7 +11,7 @@
     {
         public static WaitingCoroutineRegisterNotifier Instance { get; private set; }
 
-        public abstract void RegisterWaitingCoroutine<T>(ref T awaiter, AwaitableCoroutineBase coroutine);
+        public abstract bool RegisterWaitingCoroutine<T>(ref T awaiter, AwaitableCoroutineBase coroutine);
 
         public static void Register<T>(ref T awaiter, AwaitableCoroutineBase coroutine)
         {
@@ -21,12 +23,39 @@
 
         public sealed class Default : WaitingCoroutineRegisterNotifier
         {
-            public override void RegisterWaitingCoroutine<T>(ref T awaiter, AwaitableCoroutineBase coroutine)
+            public override bool RegisterWaitingCoroutine<T>(ref T awaiter, AwaitableCoroutineBase coroutine)
             {
                 if (awaiter is IWaitingCoroutineRegisterer registerer)
                 {
                     registerer.RegisterWaitingCoroutine(coroutine);
+                    return true;
                 }
+
+                return false;
+            }
+        }
+
+        public sealed class Composed : WaitingCoroutineRegisterNotifier
+        {
+            private readonly WaitingCoroutineRegisterNotifier[] _notifiers;
+
+            public Composed(WaitingCoroutineRegisterNotifier[] notifiers)
+            {
+                if (notifiers is null)
+                {
+                    ThrowHelper.ArgNull(nameof(notifiers));
+                }
+                _notifiers = notifiers;
+            }
+
+            public override bool RegisterWaitingCoroutine<T>(ref T awaiter, AwaitableCoroutineBase coroutine)
+            {
+                foreach (var n in _notifiers)
+                {
+                    if (n.RegisterWaitingCoroutine<T>(ref awaiter, coroutine)) return true;
+                }
+
+                return false;
             }
         }
     }
